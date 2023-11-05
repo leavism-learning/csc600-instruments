99fc8ca0d74265885cb066f7375f98e317bbd827
alex-nikols <79814353+alex-nikols@users.noreply.github.com>
2023-11-05 10:38:08 -0800

    database setup

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE songs (
 
 INSERT INTO songs (id, song_title, notes) 
 VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (2,"POPCORN",  "B5 A5 B5 Gb5 D5 Gb5 B4 B5 A5 B5 Gb5 D5 Gb5 B4 B5 Db6 D6 Db6 D6 B5 Db6 B5 Db6 A5 B5 G5 B5 A5 B5");
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (3,"Megalovania",  "D4 D4 D5 A4 G4 F4 D4 F4 G4 E4 E4 D5 A4 G4 F4 D4 F4 G4 D4 D4 D5 A4 G4 F4 D4 F4 G4 E4 E4 D5 A4 G4 F4 D4 F4 G4");
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (4,"Song of storm's", "Gb5 B4 Db5 Gb5 B4 Db5 Gb5 D5 Gb5 Db5 Gb5 D5 B4 Gb5 Db5 Gb5 B4 Gb5 Db5 Gb5 D5 Gb5 Db5 Gb5 B4 Db5 Gb5 B4 Db5 A4 B4 Gb5");
