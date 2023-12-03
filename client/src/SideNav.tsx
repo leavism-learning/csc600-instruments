11fa8758e22518605b5d9c907b5b958c29912040
Huy Dang <ghuydang@gmail.com>
2023-12-03 11:58:52 -0800

    Implemented search and popovers for song details

--- client/src/SideNav.tsx
+++ client/src/SideNav.tsx
@@ -1,12 +1,12 @@
 // 3rd party library imports
 import classNames from 'classnames';
 import { List } from 'immutable';
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import {
-  RadioButton20,
-  RadioButtonChecked20,
-  Music20,
+	RadioButton20,
+	RadioButtonChecked20,
+	Music20,
 } from '@carbon/icons-react';
 
 // project imports
@@ -14,186 +14,225 @@ import { DispatchAction } from './Reducer';
 import { AppState } from './State';
 import { Instrument } from './Instruments';
 import { Visualizer } from './Visualizers';
-
+import { OverlayTrigger, Popover } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /** ------------------------------------------------------------------------ **
  * SideNav component
  ** ------------------------------------------------------------------------ */
 
 type SideNavProps = {
-  state: AppState;
-  dispatch: React.Dispatch<DispatchAction>;
+	state: AppState;
+	dispatch: React.Dispatch<DispatchAction>;
 };
 
 export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
-  /**
-   * 
-   * SideNav
-   * |-----------------|
-   * | Nameless App    |
-   * | |-----------|   |
-   * | |           |   |
-   * | |-----------|   |
-   * |                 |
-   * | InstrumentsNav  |
-   * | |-----------|   |
-   * | |           |   |
-   * | |-----------|   | 
-   * |                 |
-   * | VisualizersNav  |
-   * | |-----------|   |
-   * | |           |   |
-   * | |-----------|   |
-   * |                 |
-   * | SongsNav        |
-   * | |-----------|   |
-   * | |           |   |
-   * | |-----------|   |
-   * |                 |
-   * |-----------------|
-  */
+	/**
+	 *
+	 * SideNav
+	 * |-----------------|
+	 * | Nameless App    |
+	 * | |-----------|   |
+	 * | |           |   |
+	 * | |-----------|   |
+	 * |                 |
+	 * | InstrumentsNav  |
+	 * | |-----------|   |
+	 * | |           |   |
+	 * | |-----------|   |
+	 * |                 |
+	 * | VisualizersNav  |
+	 * | |-----------|   |
+	 * | |           |   |
+	 * | |-----------|   |
+	 * |                 |
+	 * | SongsNav        |
+	 * | |-----------|   |
+	 * | |           |   |
+	 * | |-----------|   |
+	 * |                 |
+	 * |-----------------|
+	 */
 
-  return (
-    <div className="absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column">
-      <div className="h3 fw7 f5 flex items-center pl3 bb b--light-gray">
-        C-Sharps App
-      </div>
-      <div className="flex-auto">
-        <InstrumentsNav state={state} dispatch={dispatch} />
-        <VisualizersNav state={state} dispatch={dispatch} />
-        <SongsNav state={state} dispatch={dispatch} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column'>
+			<div className='h3 fw7 f5 flex items-center pl3 bb b--light-gray'>
+				C-Sharps App
+			</div>
+			<div className='flex-auto'>
+				<InstrumentsNav state={state} dispatch={dispatch} />
+				<VisualizersNav state={state} dispatch={dispatch} />
+				<SongsNav state={state} dispatch={dispatch} />
+			</div>
+		</div>
+	);
 }
-
 
 /** ------------------------------------------------------------------------ **
  * SideNav Sub-Components
  ** ------------------------------------------------------------------------ */
 
- function InstrumentsNav({ state }: SideNavProps): JSX.Element {
-  /** 
-   *  InstrumentsNav
-   *  |-----------------|
-   *  | Section         |
-   *  | |-------------| |
-   *  | | RadioButton | |
-   *  | |-------------| | 
-   *  | | RadioButton | |
-   *  | |-------------| |
-   *  |      ...        |
-   *  |-----------------|
-  */
-  
-  const instruments: List<Instrument> = state.get('instruments');
-  const activeInstrument = state.get('instrument')?.name;
-  const location = useLocation();
+function InstrumentsNav({ state }: SideNavProps): JSX.Element {
+	/**
+	 *  InstrumentsNav
+	 *  |-----------------|
+	 *  | Section         |
+	 *  | |-------------| |
+	 *  | | RadioButton | |
+	 *  | |-------------| |
+	 *  | | RadioButton | |
+	 *  | |-------------| |
+	 *  |      ...        |
+	 *  |-----------------|
+	 */
 
-  return (
-    <Section title="Instruments">
-      {instruments.map(i => (
-        <RadioButton
-          key={i.name}
-          to={`/${i.name}${location.search}`}
-          text={i.name}
-          active={i.name === activeInstrument}
-          onClick={() => console.log(i.name)}
-        />
-      ))}
-    </Section>
-  );
+	const instruments: List<Instrument> = state.get('instruments');
+	const activeInstrument = state.get('instrument')?.name;
+	const location = useLocation();
+
+	return (
+		<Section title='Instruments'>
+			{instruments.map((i) => (
+				<RadioButton
+					key={i.name}
+					to={`/${i.name}${location.search}`}
+					text={i.name}
+					active={i.name === activeInstrument}
+					onClick={() => console.log(i.name)}
+				/>
+			))}
+		</Section>
+	);
 }
 
 function VisualizersNav({ state }: SideNavProps): JSX.Element {
-  /** 
-   *  VisualizersNav
-   *  |-----------------|
-   *  | Section         |
-   *  | |-------------| |
-   *  | | RadioButton | |
-   *  | |-------------| | 
-   *  | | RadioButton | |
-   *  | |-------------| |
-   *  |      ...        |
-   *  |-----------------|
-  */
+	/**
+	 *  VisualizersNav
+	 *  |-----------------|
+	 *  | Section         |
+	 *  | |-------------| |
+	 *  | | RadioButton | |
+	 *  | |-------------| |
+	 *  | | RadioButton | |
+	 *  | |-------------| |
+	 *  |      ...        |
+	 *  |-----------------|
+	 */
 
-  const visualizers: List<Visualizer> = state.get('visualizers');
-  const activeVisualizer = state.get('visualizer')?.name;
-  const location = useLocation();
+	const visualizers: List<Visualizer> = state.get('visualizers');
+	const activeVisualizer = state.get('visualizer')?.name;
+	const location = useLocation();
 
-  return (
-    <Section title="Visualizers">
-      {visualizers.map(v => (
-        <RadioButton
-          key={v.name}
-          to={{
-            pathname: location.pathname,
-            search: `?visualizer=${v.name}`,
-          }}
-          text={v.name}
-          active={v.name === activeVisualizer}
-          onClick={() => console.log(v.name)}
-        />
-      ))}
-    </Section>
-  );
+	return (
+		<Section title='Visualizers'>
+			{visualizers.map((v) => (
+				<RadioButton
+					key={v.name}
+					to={{
+						pathname: location.pathname,
+						search: `?visualizer=${v.name}`,
+					}}
+					text={v.name}
+					active={v.name === activeVisualizer}
+					onClick={() => console.log(v.name)}
+				/>
+			))}
+		</Section>
+	);
 }
 
 function SongsNav({ state, dispatch }: SideNavProps): JSX.Element {
-  /** 
-   * 
-   *  SongsNav
-   *  |-----------------|
-   *  | Section         |
-   *  | |-------------| |
-   *  | | Music20     | |
-   *  | |-------------| | 
-   *  | | Music20     | |
-   *  | |-------------| |
-   *  |      ...        |
-   *  |-----------------|
-  */
-  const [displayArtist, setDisplayArtist] = useState(false);
+	/**
+	 *
+	 *  SongsNav
+	 *  |-----------------|
+	 *  | Section         |
+	 *  | |-------------| |
+	 *  | | Music20     | |
+	 *  | |-------------| |
+	 *  | | Music20     | |
+	 *  | |-------------| |
+	 *  |      ...        |
+	 *  |-----------------|
+	 */
 
-  const toggleDisplay = () => {
-    setDisplayArtist((prevDisplayArtist) => !prevDisplayArtist);
-  };
+	const popover = (title: string, author: string, album: string) => (
+		<Popover id='popover-basic'>
+			<Popover.Header as='h3'>{title}</Popover.Header>
+			<Popover.Body>
+				<strong>Author: </strong>
+				{author}
+				<br />
+				<strong>Album: </strong>
+				{album}
+			</Popover.Body>
+		</Popover>
+	);
 
+	const songs: List<any> = state.get('songs', List());
+	const [search, setSearchTerms] = React.useState('');
 
-  const songs: List<any> = state.get('songs', List());
-  return (
-    <Section title="Playlist">
-      <button onClick={toggleDisplay}>
-        {displayArtist ? 'Show Song Title' : 'Show Artist Name'}
-      </button>
-      {songs.map(song => (
-        <div
-          key={song.get('id')}
-          className="f6 pointer underline flex items-center no-underline i dim"
-          onClick={() =>
-            dispatch(new DispatchAction('PLAY_SONG', { id: song.get('id') }))
-          }
-        >
-          {displayArtist ? (
-            <>
-              <Music20 className="mr1" />
-              {song.get('songArtist')}
-            </>
-          ) : (
-            <>
-              <Music20 className="mr1" />
-              {song.get('songTitle')}
-            </>
-          )}
-        </div>
-      ))}
-    </Section>
-    //ToDo: modify to display artist name instead of song name with button.
-  );
+	const find = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchTerms(event.target.value);
+	};
+
+	return (
+		<Section title='Playlist'>
+			<Form>
+				<Form.Group className='mb-3'>
+					<Form.Control
+						type='text'
+						value={search}
+						onChange={find}
+						placeholder='Search'
+					></Form.Control>
+				</Form.Group>
+			</Form>
+			<div className='dt'>
+				{songs
+					.filter(
+						(song) =>
+							song
+								.get('songTitle')
+								.toLowerCase()
+								.includes(search.toLowerCase()) ||
+							song
+								.get('songArtist')
+								.toLowerCase()
+								.includes(search.toLowerCase()) ||
+							song.get('songAlbum').toLowerCase().includes(search.toLowerCase())
+					)
+					.map((song) => {
+						return (
+							<OverlayTrigger
+								placement='auto'
+								containerPadding={20}
+								overlay={popover(
+									song.get('songTitle'),
+									song.get('songAlbum'),
+									song.get('songArtist')
+								)}
+							>
+								<div
+									key={song.get('id')}
+									className='f6 pointer underline flex items-center no-underline i dim'
+									onClick={() =>
+										dispatch(
+											new DispatchAction('PLAY_SONG', { id: song.get('id') })
+										)
+									}
+								>
+									<Music20 className='mr1' />
+									{song.get('songTitle')}
+								</div>
+							</OverlayTrigger>
+						);
+					})}
+			</div>
+		</Section>
+	);
 }
-
 
 /** ------------------------------------------------------------------------ **
  * Auxilliary components
@@ -203,41 +242,45 @@ function SongsNav({ state, dispatch }: SideNavProps): JSX.Element {
  * Radio Button
  ** ------------------------------------- */
 
- type RadioButtonProps = {
-  to: any,
-  text: string,
-  active: boolean,
-  onClick: () => void
+type RadioButtonProps = {
+	to: any;
+	text: string;
+	active: boolean;
+	onClick: () => void;
 };
 
-function RadioButton({ to, text, active, onClick }: RadioButtonProps): JSX.Element {
-  return (
-    <Link to={to} className="no-underline">
-      <div
-        className={classNames('f6 flex items-center black', { fw7: active })}
-        onClick={onClick}
-      >
-        {active ? (
-          <RadioButtonChecked20 className="mr1" />
-        ) : (
-          <RadioButton20 className="mr1" />
-        )}
-        <div className="dim">{text}</div>
-      </div>
-    </Link>
-  );
+function RadioButton({
+	to,
+	text,
+	active,
+	onClick,
+}: RadioButtonProps): JSX.Element {
+	return (
+		<Link to={to} className='no-underline'>
+			<div
+				className={classNames('f6 flex items-center black', { fw7: active })}
+				onClick={onClick}
+			>
+				{active ? (
+					<RadioButtonChecked20 className='mr1' />
+				) : (
+					<RadioButton20 className='mr1' />
+				)}
+				<div className='dim'>{text}</div>
+			</div>
+		</Link>
+	);
 }
-
 
 /** ------------------------------------- **
  * Section
  ** ------------------------------------- */
 
 const Section: React.FC<{ title: string }> = ({ title, children }) => {
-  return (
-    <div className="flex flex-column h-25 bb b--light-gray pa3">
-      <div className="fw7 mb2">{title} </div>
-      <div className="flex-auto overflow-scroll">{children}</div>
-    </div>
-  );
+	return (
+		<div className='flex flex-column h-25 bb b--light-gray pa3'>
+			<div className='fw7 mb2'>{title} </div>
+			<div className='flex-auto overflow-scroll'>{children}</div>
+		</div>
+	);
 };
